4bc106730d440e4fe3304ae20236f9730512d1c3
Jacob Kilmer <jtkilmer@yahoo.com>
2023-01-31 16:41:04 -0800

    first commit

--- js/script.js
+++ js/script.js
@@ -1,43 +1,117 @@
-/* Picture-App JS */
-$(document).ready(function() {
+/* Picture App 2 JS */
 
+if (document.readState = "loaded") {
 
-  /*  PICTURE APP JS  */
-  /* Use paramaters images container id or class, individual images class, individual images id minus the number */
-
-  function pictureApp(imgBox, img, imgId) {
-
-  var pictureBox = $(imgBox);
-  var pictureBoxInner = $(img);
-  var pictureAmount = pictureBoxInner.length;
-  var i;
-  var imgEl;
-  i = 0;
-  imgEl = imgId;
-  imgEl = imgEl + (i + 1);
-
-  $(imgEl).fadeIn();
-
-    setInterval(
-      function() {
-        console.log(i);
-        if (i == (pictureAmount - 1)) {
-          i = 0;
-          $(imgEl).fadeOut();
-          imgEl = imgId + (i + 1);
-          console.log(i + ' ' + imgEl);
-          $(imgEl).fadeIn();
-        }
-        else if (i < (pictureAmount - 1)) {
-            i = i + 1;
-            $(imgEl).fadeOut();
-            imgEl = imgId + (i + 1);
-            console.log(i + ' ' + imgEl);
-            $(imgEl).fadeIn();
-        }
-    }, 7500);
+  function createAlbumList(item, index) {
+    var albumNameOption = document.createElement('option');
+    albumList.appendChild(albumNameOption);
+    albumNameOption.setAttribute('value', item[0]);
+    var albumNameText = document.createTextNode(item[0]);
+    albumNameOption.appendChild(albumNameText);
   }
 
-pictureApp('#picBox', '.picBoxInner', '#picBoxInner');
+  albumNames.forEach(createAlbumList);
 
-});
+  var theClock;
+
+  function pictureApp(imgBox, imgId, alNa, time, array) {
+    var outerCont = document.getElementById('theContainer');
+    var album = alNa;
+    var alArray = array;
+    var photoNumber;
+    var picBox = document.getElementById(''+ imgBox + '');
+    var imgEl;
+    var i, p;
+    i = 1;
+    p = 1;
+    imgEl = imgId;
+    imgEl = imgEl + (i + 1);
+
+    function findNumber(item, index) {
+      var itemName = album;
+      if(item[0] == itemName) {
+        photoNumber = item[1];
+      }
+    }
+    alArray.forEach(findNumber);
+
+    outerCont.setAttribute('class', 'choosen');
+
+    if (picBox.classList.contains('activated')) {
+      var child = picBox.lastElementChild;
+      clearInterval(theClock);
+      while (child) {
+        picBox.removeChild(child);
+        child = picBox.lastElementChild;
+      }
+    }
+    else {
+      picBox.setAttribute('class', 'activated');
+    }
+
+    for (p = 1; p < (photoNumber - (-1)); p++) {
+      var box = document.createElement("div");
+      box.setAttribute('id', 'picBoxInner' + p);
+      box.setAttribute('class', 'picBoxInner');
+      document.getElementById("picBox").appendChild(box);
+      document.getElementById('picBoxInner' + p).style.backgroundImage = 'url(photo_albums/' + album + '/' + p + '.jpg)';
+    }
+
+    document.getElementById('picBoxInner' + i).style.opacity = 1;
+
+    if (theClock == null) {
+
+        theClock = setInterval(function() {
+          if (i <= photoNumber) {
+              if (i == photoNumber) {
+                document.getElementById('picBoxInner' + i).style.opacity = 0;
+                i = 1;
+                document.getElementById('picBoxInner' + i).style.opacity = 1;
+              }
+              else {
+                document.getElementById('picBoxInner' + i).style.opacity = 0;
+                document.getElementById('picBoxInner' + (i + 1)).style.opacity = 1;
+                i = i + 1;
+              }
+          }
+      }, 6000);
+    }
+    else {
+      clearInterval(theClock);
+      theClock = setInterval(function() {
+        if (i <= photoNumber) {
+            if (i == photoNumber) {
+              document.getElementById('picBoxInner' + i).style.opacity = 0;
+              i = 1;
+              document.getElementById('picBoxInner' + i).style.opacity = 1;
+            }
+            else {
+              document.getElementById('picBoxInner' + i).style.opacity = 0;
+              document.getElementById('picBoxInner' + (i + 1)).style.opacity = 1;
+              i = i + 1;
+            }
+        }
+    }, 6000);
+    }
+}
+//pictureApp
+
+  var imageButton = document.getElementById('image_changer');
+  var imageButtonBack = document.getElementById('menuBack');
+  var imageButtonExpander = document.getElementById('image_expander');
+  imageButton.onclick = function(){
+    var albumChange = document.getElementById('albumList').value;
+    pictureApp('picBox', '#picBoxInner', albumChange, theClock, albumNames);
+  }
+  imageButtonBack.onclick = function(){
+    var j = document.getElementById('theContainer');
+    j.classList.remove('choosen');
+    j.classList.add('menu_out');
+  }
+  imageButtonExpander.onclick = function(){
+    var k = document.getElementById('theContainer');
+    k.classList.remove('menu_out');
+    k.classList.add('choosen');
+  }
+
+}// loaded document
---
+++ read_me.txt
@@ -0,0 +1,38 @@
+Picture app 2
+
+~ To use Picture app 2, open with your favourite browser the "index.html" file in the "picture_app_2" folder.
+
+  - if you've never done so, right click and select "open with" or similar to choose your internet browser(chrome, firefox, internet explorer, safari).
+
+
+
+~ To add new photo albums
+
+Step #1
+
+- add your existing or new photo album to the "photo_albums" folder in the "picture_app_2" folder. Makes sure all album names have no spaces.
+
+  example:
+
+     bad : "album 1"
+     good: "album1" or "album_1" or "album-1"
+
+
+Step #2
+
+- number all photos in album "1 - ????". See examples in photo_albums folder.
+
+- make sure all photos are .jpg
+
+  example:
+
+     bad : "pic0008.jpg" or "pic008.png"
+     good : "8.jpg"
+
+
+Step #3
+
+Log in your new album and picture total in the "index.html" file.
+
+- Double click index.html in your "picture_app_2" folder and log in your new album in the "albumNames" array at the bottom.
+  When you're new album is added don't forget to save your changes to index.html by going to file and clicking save.
